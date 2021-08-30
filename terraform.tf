79a966e0bad15940c51d3b35ba2ddeee6ce9e428
LucasBurgessDev <lucasburgess8@gmail.com>
2021-08-30 21:48:36 +0100

    RDS added

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,15 @@
 terraform {
   required_version = ">= 0.15.4"
 
-required_providers {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.42"
+    }
   }
-}
 }
 
 provider "aws" {
-    profile = "default"
-    region  = "eu-west-2"
+  profile = "default"
+  region  = "eu-west-2"
 }
